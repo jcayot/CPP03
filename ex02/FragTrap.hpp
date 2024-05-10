73157b97d143ac41ceb6c3024412a43506974f7b
jules <jules.cayot@ik.me>
2024-05-10 21:06:48 +0300

    ex02

---
+++ ex02/FragTrap.hpp
@@ -0,0 +1,23 @@
+//
+// Created by jules on 10/05/2024.
+//
+
+#ifndef FRAGTRAP_HPP
+#define FRAGTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class FragTrap : ClapTrap {
+	public:
+		FragTrap();
+		FragTrap(const std::string &name);
+		FragTrap(const FragTrap &other);
+		~FragTrap();
+
+		FragTrap &operator=(const FragTrap &other);
+
+		void	highFivesGuys(void);
+};
+
+
+#endif //FRAGTRAP_HPP
